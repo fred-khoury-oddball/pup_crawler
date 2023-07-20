0e16400088f5bfda246bdf726a1bb3d8bf3a8581
fk <fk@fks-MacBook-Pro.local>
2023-07-20 16:24:30 -0500

    remove pdf packages our pdfs are displayed as webpages

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
-const pdf = require('pdf-parse');
-puppeteer.use(StealthPlugin());
+const path = require('path');
 
+puppeteer.use(StealthPlugin());
+//require('events').EventEmitter.defaultMaxListeners = 10000; // Or another suitable number
 const visitedUrls = new Set();
 const foundUrls = [];
 const failedUrls = [];
 const totalLinks = [];
 let linksVisited = 0;
 const BASE_URL = 'https://staging.va.gov/';
+const PROD_URL = 'https://www.va.gov';
 const startTime = Date.now();
-
+let browser = null;
 async function crawlPage(url) {
-    const browser = await puppeteer.launch({headless:'new'});
+    if (!browser) {
+        browser = await puppeteer.launch({ headless: 'new' });
+    }
     const page = await browser.newPage();
     if (visitedUrls.has(url)) {
+        page.close()
         return;
     }
 
     if (url.match(/\.(png|jpg|jpeg)$/)) {
         console.log(`Skipping non-HTML page ${url}`);
         fs.appendFileSync(`${startTime}/logs.txt`, `Skipping non-HTML page ${url}`);
+        page.close()
         return;
     }
 
     if (url.includes('#content')) {
         console.log(`Skipping url with #content ${url}`);
         fs.appendFileSync(`${startTime}/logs.txt`, `Skipping url with #content ${url}`);
+        page.close()
+        return;
+    }
+    //skipping all urls with query strings
+    if (url.includes('?')) {
+        console.log(`Skipping url with query string ${url}`);
+        fs.appendFileSync(`${startTime}/logs.txt`, `Skipping url with ?next query string ${url}`);
+        page.close()
         return;
     }
 
-    if (url.includes('?next')) {
-        console.log(`Skipping url with ?next query string ${url}`);
-        fs.appendFileSync(`${startTime}/logs.txt`, `Skipping url with ?next query string ${url}`);
-        return;
-    }
     console.log(`There are currently ${totalLinks.length - linksVisited} links left to visit`)
     fs.appendFileSync(`${startTime}/logs.txt`, `There are currently ${totalLinks.length - linksVisited} links left to visit`)
     console.log(`Crawling page ${url}`);
@@ -46,40 +55,21 @@ async function crawlPage(url) {
     try {
         await page.goto(url, { timeout: 15000 });
         await page.waitForTimeout(5000);
-
         const pageText = await page.evaluate(() => document.body.innerText);
         if (pageText.includes("VA.gov isn't working right now")) {
             fs.appendFileSync(`${startTime}/needs_manual_review.txt`, `${url}\n`);
+            return;
         }
+        links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), a => a.href));
 
-        if (url.endsWith('.pdf')) {
-            const buffer = await response.buffer();
-            const data = await pdf(buffer);
-
-            links = data.text.match(/\bhttps?:\/\/\S+/gi);
-            for (let pdfUrl of links) {
-                const linkToFind = ['/application/527EZ/introduction', '/burials-and-memorials/application/530/introduction'];
-                for (let targetLink of linkToFind) {
-                    if (pdfUrl.includes(targetLink) && !foundUrls.includes(pdfUrl)) {
-                        console.log(`Found target link in PDF ${pdfUrl}`);
-                        fs.appendFileSync(`${startTime}/logs.txt`, `Found target link in PDF ${pdfUrl}`);
-                        foundUrls.push(pdfUrl);
-                        fs.appendFileSync(`${startTime}/found_urls.txt`, `\n${pdfUrl} || ${targetLink}`);
-                    }
-                }
-            }
-        } else {
-            links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), a => a.href));
-
-            const linkToFind = ['/application/527EZ/introduction', '/burials-and-memorials/application/530/introduction'];
-            for (let link of links) {
-                for (let targetLink of linkToFind) {
-                    if (link.includes(targetLink) && !foundUrls.includes(link)) {
-                        console.log(`Found target link on page ${link}`);
-                        fs.appendFileSync(`${startTime}/logs.txt`, `Found target link on page ${link}`);
-                        foundUrls.push(link);
-                        fs.appendFileSync(`${startTime}/found_urls.txt`, `\n${link} || ${targetLink}`);
-                    }
+        const linkToFind = ['/application/527EZ/introduction', '/burials-and-memorials/application/530/introduction'];
+        for (let link of links) {
+            for (let targetLink of linkToFind) {
+                if (link.includes(targetLink) && !foundUrls.includes(link)) {
+                    console.log(`Found target link on page ${link}`);
+                    fs.appendFileSync(`${startTime}/logs.txt`, `Found target link on page ${link}`);
+                    foundUrls.push(link);
+                    fs.appendFileSync(`${startTime}/found_urls.txt`, `\n${link} || ${targetLink}`);
                 }
             }
         }
@@ -89,7 +79,7 @@ async function crawlPage(url) {
         failedUrls.push(url);
         fs.appendFileSync(`${startTime}/failed_urls.txt`, `\n${url}`);
     } finally {
-        await browser.close();
+        await page.close();
     }
 
     for (let link of links) {
@@ -99,7 +89,10 @@ async function crawlPage(url) {
     }
     // Spawn a new browser for each unvisited link.
     for (let link of links) {
-        if (link.startsWith(BASE_URL) && !visitedUrls.has(link)) {
+        if (link.startsWith(PROD_URL)) {
+            console.log(`PROD url found ${url}`);
+            fs.appendFileSync(`${startTime}/prod_urls_found.txt`, `${url}`);
+        } else if (link.startsWith(BASE_URL) && !visitedUrls.has(link)) {
             await crawlPage(link);
         }
     }
@@ -113,6 +106,7 @@ async function runCrawler() {
         console.error(err);
     }
     await crawlPage(BASE_URL);
+    browser.close()
 }
 
 runCrawler();
