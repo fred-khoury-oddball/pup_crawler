9662fa6ffa70eb8d7ca3bdb66c3e4415cdeb5cf1
fk <fk@fks-MacBook-Pro.local>
2023-07-24 15:48:22 -0500

    added sitemap crawler

---
+++ sitemap.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const xml2js = require('xml2js');
+const fs = require('fs');
+
+// URLs to look for
+const url1 = '/application/527EZ/introduction';
+const url2 = '/burials-and-memorials/application/530/introduction';
+
+// Reading XML from file
+const xml = fs.readFileSync('./sitemap.xml');
+const startTime = Date.now();
+
+// Create log directory
+try {
+    fs.mkdirSync(`sitemap/${startTime}`, { recursive: true });
+    console.log('Directory created successfully!');
+} catch (err) {
+    console.error(err);
+}
+
+// Converting XML to JSON
+xml2js.parseString(xml, async function(err, result) {
+  if (err) {
+    console.error('Error while parsing XML:', err);
+    return;
+  }
+
+  const urls = result.urlset.url.map(u => u.loc[0].trim());
+
+  const browser = await puppeteer.launch({ headless: 'new', product: 'firefox' });
+
+  for (let url of urls) {
+    const page = await browser.newPage();
+    try {
+        pupc_log('logs.txt',`Crawling: ${url}`);
+        await page.goto(url, { timeout: 10000 });
+    
+        // Search for the specific links in the page
+        const links = await page.evaluate((url1, url2) => Array.from(document.querySelectorAll('a'))
+          .map(a => a.href)
+          .filter(href => href === url1 || href === url2), url1, url2);
+        pupc_log('logs.txt',`Found ${links.length} relevant links in ${url}`)
+        pupc_log('found_links.txt',`${url} , ${links}`)
+    } catch(err){
+        pupc_log('errors.txt',`${url} , ${err}`)
+    }
+    await page.close()    
+  }
+
+  await browser.close();
+});
+
+function pupc_log(file,message){
+    fs.appendFileSync(`sitemap/${startTime}/${file}`, `${message}\n`);
+    console.log(message)
+
+}
